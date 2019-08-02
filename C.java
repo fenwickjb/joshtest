e1d7b93ae3add975a0bc0c39f0fdc42b2b4866ae
Jay Fenwick <fenwickjb@appstate.edu>
2019-08-02 12:39:10 -0400

    dev branch added C class

---
+++ C.java
@@ -0,0 +1,9 @@
+public class C {
+    private int x;
+    public C() {
+	x = 99;
+    }
+    public String print() {
+	return "I am a C object with x="+x;
+    }
+}
--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     A();
     B();
+    C();
   }
   private static void A() {
       System.out.println("I modified A.");
@@ -9,4 +10,7 @@ public class Hello {
   private static void B() {
       System.out.println("User2 modified B.");
   }
+    private static void C() {
+	System.out.println( (new C()).print() );
+    }
 }
